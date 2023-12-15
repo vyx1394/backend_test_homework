c7ae01f4f153221cb22ede4215173ce88f866c42
Victor <vyx1394@yandex.ru>
2023-12-15 18:00:45 +0300

    complite module

--- new.py
+++ new.py
@@ -1,9 +1,21 @@
 class Employee:
-    
+    vacation_days = 28
 
-# Создайте экземпляры класса Employee с различными значениями атрибутов.
-employee1 = Employee(...)
-employee2 = Employee(...)
+    def __init__(self, first_name, second_name, gender):
+        self.first_name = first_name
+        self.second_name = second_name
+        self.gender = gender
+        self.remaining_vacation_days = self.vacation_days
+        # Сюда добавьте новый атрибут remaining_vacation_days
 
-# Допишите код для вывода информации о сотрудниках.
-print(...)
+    def consume_vacation(self, remaining_days):
+        self.remaining_days = self.vacation_days - remaining_days
+        
+    def get_vacation_details(self):
+        return f'Остаток отпускных дней: {self.remaining_days}'
+        # Сюда добавьте методы consume_vacation и get_vacation_details.
+
+
+employee = Employee('Роберт', 'Крузо', 'м')
+employee.consume_vacation(7)
+print(employee.get_vacation_details())
